b37d2ac816ff67d7d032ac316913566d71401af1
demetriustw <56927431+demetriustw@users.noreply.github.com>
2022-06-14 09:09:56 -0600

    Add files via upload

--- Node-Course/notes-app/app.js
+++ Node-Course/notes-app/app.js
@@ -1,3 +1,7 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+// Fs Introduction
+//////////////////////////////////////////////////////////////////////////////////////////
+
 // const fs = require ("fs");
 // fs.appendFileSync
 
@@ -18,11 +22,17 @@
 // module.exports = getNotes;`
 // );
 
-const getNotes = require("./notes.js");
+//////////////////////////////////////////////////////////////////////////////////////////
+// Node Introduction
+//////////////////////////////////////////////////////////////////////////////////////////
 
-const msg = getNotes();
+// npm i node@18.3.0
 
-console.log(msg);
+// const getNotes = require("./notes.js");
+
+// const msg = getNotes();
+
+// console.log(msg);
 
 //
 // const validator = require("validator");
@@ -40,9 +50,120 @@ console.log(msg);
 // Bonus: Use docs to mess around with other styles. Make text bold and inversed
 //
 
+// npm i chalk@2.4.1
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Chalk Introduction
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// const chalk = require("chalk");
+// const getNotes = require("./notes.js");
+
+// const greenMsgInv = chalk.green.bold.inverse.italic("Success!");
+// const greenMsgUl = chalk.green.bold.italic.underline("Success!");
+// const redMsg = chalk.red.inverse.bold("Error!");
+
+// console.log(greenMsgInv);
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Nodemon Introduction
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// npm i nodemon@1.12.5
+
+// console.log(process.argv[2]);
+
+// const input = process.argv[2];
+
+// if (input !== "") {
+//   console.log("Hello, " + input + "!");
+// }
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Yargs Introduction
+//////////////////////////////////////////////////////////////////////////////////////////
+
 const chalk = require("chalk");
+const yargs = require("yargs");
+const notes = require("./notes.js");
 
-const greenMsgInv = chalk.blue.bold.bgGreen.italic("Success!");
-const greenMsgUl = chalk.green.bold.italic.underline("Success!");
+// Customize yargs version
+yargs.version("1.1.0");
 
-console.log(greenMsgInv, greenMsgUl);
+// console.log(process.argv);
+
+//
+// Challenge 2: Add an option to yargs
+//
+// 1. Setup a body option for the add command
+// 2. Configure a description, make it requried, and for it to be a string
+// 3. Log the body value in the handler function
+// 4. Test your work!
+//
+
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note Body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.addNote(argv.title, argv.body);
+  },
+});
+
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing the note!");
+  },
+});
+
+//
+// Challenge 1: Add two new commands
+//
+// 1. Setup command support "list" command (print placeholder message for now)
+// 2. Setup command support "read" command (print placeholder message for now)
+// 3. Test your work by running both commands and ensure correct output
+//
+
+// Create list command
+yargs.command({
+  command: "list",
+  describe: "List all notes",
+  handler: function () {
+    console.log("Listing all the notes!");
+  },
+});
+
+// Create read command
+yargs.command({
+  command: "read",
+  describe: "Print the note",
+  handler: function () {
+    console.log("Printing the note!");
+  },
+});
+
+yargs.parse();
+// console.log(yargs.argv);
+
+// const command = process.argv[2];
+
+// if (command === "add") {
+//   console.log("Adding note!");
+// } else if (command === "remove") {
+//   console.log("Removing note!");
+// }
--- Node-Course/notes-app/notes.js
+++ Node-Course/notes-app/notes.js
@@ -1,7 +1,74 @@
-console.log("notes.js");
+// console.log("notes.js");
+const fs = require("fs");
 
 const getNotes = function () {
   return "Your notes...";
 };
 
-module.exports = getNotes;
+const addNote = function (title, body) {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(function (note) {
+    return note.title === title;
+  });
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+
+    saveNotes(notes);
+    console.log("New note added!");
+  } else {
+    console.log("Note title taken!");
+  }
+};
+
+//
+// Challenge 1: Setup command option
+//
+// 1. Setup the remove command to take a required "--title" option
+// 2. Create and export a removeNote function form notes.js
+// 2. Call removeNote in remove command handler
+// 2. Have removeNote log the title of the note to be removed
+// 3. Test your work by running both commands and ensure correct output
+//
+
+const removeNote = function (title) {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(function (note) {
+    return note.title === title;
+  });
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+
+    saveNotes(notes);
+    console.log("New note added!");
+  } else {
+    console.log("Note title taken!");
+  }
+};
+
+const saveNotes = function (notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+const loadNotes = function () {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+};
